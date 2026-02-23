f93e5c1e6848185b1e09cae68888d5fe6cb260c3
Philip Riecks <mail@philipriecks.de>
2026-02-23 07:45:26 +0100

    lab-1

--- labs/lab-1/src/test/java/pragmatech/digital/workshops/lab1/experiment/JUnitExtensionTest.java
+++ labs/lab-1/src/test/java/pragmatech/digital/workshops/lab1/experiment/JUnitExtensionTest.java
@@ -3,7 +3,7 @@ package pragmatech.digital.workshops.lab1.experiment;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(TimingExtension.class)
+//@ExtendWith(TimingExtension.class)
 class JUnitExtensionTest {
 
   @Test
